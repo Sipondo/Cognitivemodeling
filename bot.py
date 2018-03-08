f80fb770372de7c8b20393e3244dbef44fee895e
Sipondo <tt_robroek@hotmail.com>
2018-03-08 09:24:47 +0100

    gitignore

--- bot.py
+++ bot.py
@@ -3,8 +3,10 @@ import telebot
 
 bot = telebot.TeleBot("456213688:AAFoo1ONApk_f0xmd0QDME0itr60lI90fSs")
 
-@bot.message_handler(commands=['start', 'help'])
+#commands=['start', 'help']
+@bot.message_handler()
 def send_welcome(message):
     bot.reply_to(message, u"Hello, welcome to this bot!")
+    print(message)
 
 bot.polling()
