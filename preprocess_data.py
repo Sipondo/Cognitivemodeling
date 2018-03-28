81bccde88f98f400f17c182a8bc3f9b1d7e7eb90
Ties Robroek <tt_robroek@hotmail.com>
2018-03-28 14:14:04 +0200

    bindings

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,5 +1,11 @@
 #import unicodecsv as csv
 import csv
+import wikipedia
+import pprint
+import urllib
+import urllib.request
+import time
+from tqdm import tqdm
 
 author_dict = {}
 
@@ -8,36 +14,79 @@ with open("pol_accounts.csv", encoding="utf-8") as file:
     for row in reader:
         author_dict[int(row["id"])] = ([row["screen_name"], row["description"]])
 
-bobby = author_dict[785624983145852928]
+def get_search_string(representative):
+    return representative[0] + " " + representative[1]
+
+binding_list = []
+with open("pol_bindings.csv") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        binding_list.append(int(row["id"]))
 
 def get_search_string(representative):
     return representative[0] + " " + representative[1]
 
-get_search_string(bobby)
+with open("pol_bindings.csv",'a') as file:
+    writer = csv.DictWriter(file, fieldnames=["id","screen_name","democrat","republican"])
+    #writer.writeheader()
+    for row in tqdm(author_dict):
+        if not row in binding_list:
+            time.sleep(3)
+            user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+            url = "https://www.google.nl/search?q=wikipedia+" + author_dict[row][0]
+            headers={'User-Agent':user_agent,}
 
-from wikidata.client import Client
-import wikipedia
+            request=urllib.request.Request(url,None,headers)
+            response = urllib.request.urlopen(request)
+            data = str(response.read())
+            responses = str(data).split("wikipedia.org/wiki/")[1:]
+            democrat = -1
+            republican = -1
+            if len(responses)>0:
+                handle = wikipedia.search(responses[0].split("&")[0])
+                if len(handle)>0:
+                    try:
+                        page = wikipedia.page(handle[0])#
+                        searchpage = page.content.lower()
 
-client = Client()
+                        democrat = searchpage.count('democrat')
+                        republican = searchpage.count('republic')
 
-wikipedia.search("repbobbyscott")
+                        if(searchpage.find('democrat')<searchpage.find('republican')):
+                            democrat = democrat + 100
 
-entity = client.get('repbobbyscott')
+                        if(searchpage.find('democrat')>searchpage.find('republican')):
+                            republican = republican + 100
+                    except Exception:
+                        pass
+            writer.writerow({"id": row, "screen_name": author_dict[row][0], "democrat": democrat, "republican": republican})
 
-entity.description
+
+
+
+
+# from googleapiclient.discovery import build
+#
+# service = build("customsearch", "v1",
+#         developerKey="AIzaSyD1QS165pR5x2VmMGjneXL3zERNW8WdJCY")
+#
+# #001432505923933560789:cbh8sypy4h4
+# #017576662512468239146:omuauf_lfve
+# res = service.cse().list(q='bobbyscott', cx='017576662512468239146:omuauf_lfve',).execute()
+# pprint.pprint(res)
 
 
     # Sleutel 1: 93a63c2d291f4148a548bd4741b4d833
     #
     # Sleutel 2: 3894e4f9af4b483aa730dc34c42f6610
 ####For Web Results:
-
-
-from py_ms_cognitive import PyMsCognitiveWebSearch
-search_term = "Python Software Foundation"
-search_service = PyMsCognitiveWebSearch('93a63c2d291f4148a548bd4741b4d833', search_term)
-
-first_fifty_result = search_service.search(limit=50, format='json') #1-50
+#
+#
+# from py_ms_cognitive import PyMsCognitiveWebSearch
+# search_term = "Python Software Foundation"
+# search_service = PyMsCognitiveWebSearch('93a63c2d291f4148a548bd4741b4d833', search_term)
+#
+# first_fifty_result = search_service.search(limit=50, format='json') #1-50
 # >>> second_fifty_resul t= search_service.search(limit=50, format='json') #51-100
 #
 # >>> print (second_fifty_result[0].snippet)
